c7b4693c8e8088f2fc082b73f0df561e60fc89aa
Kajetan Bojko <kai@shutl.co.uk>
2014-01-05 21:19:56 +0000

    beat all 5 house bots

--- spec/match_spec.rb
+++ spec/match_spec.rb
@@ -21,26 +21,6 @@ describe Match do
     end
   end
 
-  describe 'playing dynamite or waterbomb after tie' do
-    let(:random_move_generator) do
-      sequence = %w(SCISSORS SCISSORS SCISSORS)
-      lambda { sequence.pop }
-    end
-
-    specify do
-      subject.move.should == 'SCISSORS'
-      subject.opponent_move('SCISSORS')
-
-      subject.move.should == 'DYNAMITE'
-      subject.opponent_move('DYNAMITE')
-
-      subject.move.should == 'DYNAMITE'
-      subject.opponent_move('DYNAMITE')
-
-      subject.move.should == 'SCISSORS'
-    end
-  end
-
   describe 'no blow ups' do
     let(:random_move_generator) { nil }
 
